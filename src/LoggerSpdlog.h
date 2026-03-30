32bf9cc47c264234dc5a2b6d5fc67413e69565b8
fog_java <fog_java@126.com>
2026-03-30 14:06:30 +0000

    add spdlog

---
+++ src/LoggerSpdlog.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "LoggerModule.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <memory>
+class LoggerSpdlog : public LoggerModule {
+ public:
+    void init(int argc, char* argv[]) override final {
+       logger_ = spdlog::basic_logger_mt("file_logger", "/home/vboxuser/spdlog.txt");
+       logger_->set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
+    }
+
+    void log_info(const std::string& msg) override final {
+        logger_->info(msg);
+    }
+
+    void shutdown() override final {
+        spdlog::shutdown();
+    }
+ private:
+    std::shared_ptr<spdlog::logger> logger_;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "benchmark.h"
 #include "LoggerGlog.h"
+#include "LoggerSpdlog.h"
 #include "BenchmarkRunner.h"
 int main(int argc, char** argv) {
 /*
@@ -12,5 +13,11 @@ int main(int argc, char** argv) {
    BenchmarkRunner runner(&logger, 4, 100000);
    runner.run();
    logger.shutdown();
+
+   LoggerSpdlog logger1;
+   logger1.init(argc, argv);
+   BenchmarkRunner runner1(&logger1, 4, 100000);
+   runner1.run();
+   logger1.shutdown();
    return 0;
 }
